b3949f07dfc1af82606206cb3ab1e19a377568e1
Orange Dev <94748125+orangedevs@users.noreply.github.com>
2022-10-22 14:28:01 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+const music_list = [
+    {
+        img : 'images/stay.png',
+        name : 'Stay',
+        artist : 'The Kid LAROI, Justin Bieber',
+        music : 'music/stay.mp3'
+    },
+    {
+        img : 'images/fallingdown.jpg',
+        name : 'Falling Down',
+        artist : 'Wid Cards',
+        music : 'music/fallingdown.mp3'
+    },
+    {
+        img : 'images/faded.png',
+        name : 'Faded',
+        artist : 'Alan Walker',
+        music : 'music/Faded.mp3'
+    },
+    {
+        img : 'images/ratherbe.jpg',
+        name : 'Rather Be',
+        artist : 'Clean Bandit',
+        music : 'music/Rather Be.mp3'
+    },
+    {
+        img : 'images/xzx.jpeg',
+        name : '雪中行 - 伴奏',
+        artist : '张若昀',
+        music : 'music/xzhdx.mp3'
+    },
+    {
+        img : 'images/xzx.jpeg',
+        name : '雪中行',
+        artist : '张若昀',
+        music : 'music/xzx.mp3'
+    },
+    {
+        img : 'images/xzx.jpeg',
+        name : '惶惶北椋真灵歌',
+        artist : '戴荃',
+        music : 'music/hhblzlg.mp3'
+    }
+];
